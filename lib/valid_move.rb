567108118aa9fa1e8ec0978f7ae852910b6fc9b7
Amy Chin-Lai <amy.chinlai@gmail.com>
2019-11-13 21:33:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,5 @@
 def valid_move?(board, index)
-  if index.between?(0,8)
-    true
-  elsif position_taken?(board, index)
+  index.between?(0,8) && position_taken?(board, index) == false
     true
 end
 end
